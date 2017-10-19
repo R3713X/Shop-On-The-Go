c8e532b6e72933abbcb1f4aa792d49ed59e39228
ioaktheo <ioaktheo@icloud.com>
2017-10-19 17:31:42 +0300

    Edited manifest by adding a new meta tag.

--- app/src/main/java/com/sirialkillers/shoponthego/MapsActivity.java
+++ app/src/main/java/com/sirialkillers/shoponthego/MapsActivity.java
@@ -38,9 +38,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
+
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(33.852, 10.211)).title(""));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
