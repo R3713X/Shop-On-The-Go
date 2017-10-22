49a36ce38905a81db5d77482cc0b70448249995d
chrisaslamagidis <chriasla1@teiser.gr>
2017-10-22 10:29:39 +0300

    changes about gps services

--- app/src/main/java/com/sirialkillers/shoponthego/MapsActivity.java
+++ app/src/main/java/com/sirialkillers/shoponthego/MapsActivity.java
@@ -1,18 +1,32 @@
 package com.sirialkillers.shoponthego;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    DeviceLocation deviceLocation;
+    private GoogleMap mMap;
+    private BroadcastReceiver broadcastReceiver;
+    LatLng userLocation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if(!runtime_perimissions()){
+            Intent intent=new Intent(getApplicationContext(),GPS_Service.class);
+            startService(intent);
+
+        }
     }
 
 
@@ -36,6 +56,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        deviceLocation=new DeviceLocation(this,googleMap);
+        mMap = googleMap;
+        if (broadcastReceiver==null){
+            broadcastReceiver=new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+
+                    userLocation=new LatLng(intent.getExtras().getDouble("Lat"),intent.getExtras().getDouble("Long"));
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.iconbluedot)));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+
+                }
+            };
+            registerReceiver(broadcastReceiver,new IntentFilter("location update"));
+        }
+
+    }
+
+
+    private boolean runtime_perimissions() {
+        if(Build.VERSION.SDK_INT>=23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new  String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},100);
+            return true;
+
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent=new Intent(getApplicationContext(),GPS_Service.class);
+                startService(intent);
+            }
+            else {
+                runtime_perimissions();
+            }
+
+        }
     }
 }
