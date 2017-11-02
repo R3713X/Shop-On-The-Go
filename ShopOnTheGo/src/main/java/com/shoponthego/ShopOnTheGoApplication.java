8f01fb356dd6a5d73e6bac7636bd871966c8c778
dimiaman <dimiaman2@gmail.com>
2017-11-03 00:16:49 +0200

    REST Api for Shops

---
+++ ShopOnTheGo/src/main/java/com/shoponthego/ShopOnTheGoApplication.java
@@ -0,0 +1,12 @@
+package com.shoponthego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOnTheGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopOnTheGoApplication.class, args);
+	}
+}
