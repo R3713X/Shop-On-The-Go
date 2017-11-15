f8b62a49112509f62fee3d647f618574fd727731
ioaktheo <ioaktheo@icloud.com>
2017-11-15 20:02:17 +0200

    Added a temporary discount model.

---
+++ app/src/main/java/com/sirialkillers/shoponthego/Models/DiscountModel.java
@@ -0,0 +1,12 @@
+package com.sirialkillers.shoponthego.Models;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * @author Ioakeim James Theologou
+ * @version 15/11/2017
+ *
+ */
+public class DiscountModel extends ArrayList {
+}
