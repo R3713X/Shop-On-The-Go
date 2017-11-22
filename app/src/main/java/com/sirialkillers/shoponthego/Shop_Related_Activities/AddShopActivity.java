5f86b1c55e0e289ce7271df7adf111d2ce39247f
R3713X <kapsalgi@gmail.com>
2017-11-22 16:19:17 +0200

    found how google places work but we need to update the API KEY so it works, will work in desired categories for now.

--- app/src/main/java/com/sirialkillers/shoponthego/Shop_Related_Activities/AddShopActivity.java
+++ app/src/main/java/com/sirialkillers/shoponthego/Shop_Related_Activities/AddShopActivity.java
@@ -1,23 +1,35 @@
 package com.sirialkillers.shoponthego.Shop_Related_Activities;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.net.Uri;
 
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 import com.sirialkillers.shoponthego.R;
 import com.squareup.picasso.Picasso;
 
@@ -28,31 +40,38 @@ public class AddShopActivity extends AppCompatActivity {
     private EditText titleEditText;
     private ImageView shopImage;
     private TextView shopCategoriesTextView;
-    private EditText shopAddressEditText;
-    private EditText postalCodeEditText;
+    private TextView shopAddressTextView;
     private String shopCategories;
     private Address shopAddress;
+    private LatLng shopsLatLng;
 
     String sCategories;
     String[] categories;
     boolean[] checkedCategories;
     ArrayList<Integer> mShopCategories = new ArrayList<>();
 
-    private final static int REQUEST_CAMERA=1;
-    private final static int SELECT_FILE=0;
+    private final static int REQUEST_CAMERA = 1;
+    private final static int SELECT_FILE = 0;
+
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
+    private final static int PLACE_PICKER_REQUEST = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shop);
 
-        shopAddressEditText = (EditText) findViewById(R.id.shopAddressEditText);
-        postalCodeEditText = (EditText) findViewById(R.id.postCodeEditText);
+        requestPermission();
+
+        shopAddressTextView = (TextView) findViewById(R.id.addressTextView);
         shopCategoriesTextView = (TextView) findViewById(R.id.categoriesTextView);
 
         titleEditText = (EditText) findViewById(R.id.titleEditText);
 
-        shopImage =(ImageView) findViewById(R.id.shopPhotoImageView);
+        shopImage = (ImageView) findViewById(R.id.shopPhotoImageView);
+
+        categories = getResources().getStringArray(R.array.productCategories);
+        checkedCategories = new boolean[categories.length];
 
         TextView selectCategories = (TextView) findViewById(R.id.selectCategoriesTextView);
         selectCategories.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +81,15 @@ public class AddShopActivity extends AppCompatActivity {
             }
         });
 
-        
+        final TextView selectAddress = (TextView) findViewById(R.id.selectAddressTextView);
+
+        selectAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectAddressClick();
+            }
+        });
+
         Button addPhoto = (Button) findViewById(R.id.addPhotoButton);
         addPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +106,18 @@ public class AddShopActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void selectAddressClick() {
+        PlacePicker.IntentBuilder placePickerBuilder = new PlacePicker.IntentBuilder();
+        try {
+            Intent intent = placePickerBuilder.build(AddShopActivity.this);
+            startActivityForResult(intent, PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -141,23 +180,22 @@ public class AddShopActivity extends AppCompatActivity {
     }
 
     private void addPhotoClick() {
-        final CharSequence[] items={"Camera","Gallery","Cancel"};
-        AlertDialog.Builder builder=new AlertDialog.Builder(AddShopActivity.this);
+        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddShopActivity.this);
         builder.setTitle("Add Image");
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                if(items[i].equals("Camera")){
-                    Intent intent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent,REQUEST_CAMERA);
-                }
-                else if (items[i].equals("Gallery")){
-                    Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("image/*");
-                    startActivityForResult(intent.createChooser(intent,"Select File"),SELECT_FILE);
 
-                }
-                else if(items[i].equals("Cancel")){
+                if (items[i].equals("Camera")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[i].equals("Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+
+                } else if (items[i].equals("Cancel")) {
                     dialogInterface.dismiss();
 
                 }
@@ -170,25 +208,54 @@ public class AddShopActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode== Activity.RESULT_OK){
-            if (requestCode==REQUEST_CAMERA){
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
 
-                Uri selectedImageUri =data.getData();
+                Uri selectedImageUri = data.getData();
                 Picasso.with(this).load(selectedImageUri).fit().centerInside().into(shopImage);
 
+            } else if (requestCode == SELECT_FILE) {
 
-            }
-            else if(requestCode==SELECT_FILE){
-                Uri selectedImageUri =data.getData();
+                Uri selectedImageUri = data.getData();
                 Picasso.with(this).load(selectedImageUri).fit().centerInside().into(shopImage);
 
+            } else if (requestCode == PLACE_PICKER_REQUEST){
+
+                if (resultCode == RESULT_OK){
+
+                    Place place = PlacePicker.getPlace(AddShopActivity.this, data);
+                    shopAddressTextView.setText(place.getAddress());
+                    shopsLatLng=place.getLatLng();
+
+                }
             }
         }
     }
-    private void attemptSubmitShop() {
+
+    private void requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void attemptSubmitShop() {
+    }
 
 
 }
