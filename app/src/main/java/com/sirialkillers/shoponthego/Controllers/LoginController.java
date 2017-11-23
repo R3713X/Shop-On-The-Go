ea9b20290b72ae2e254139db809e99f89e55bb1e
ioaktheo <ioaktheo@icloud.com>
2017-11-23 18:31:45 +0200

    Created a Model and a Controller for the Rest Client Login feature.

---
+++ app/src/main/java/com/sirialkillers/shoponthego/Controllers/LoginController.java
@@ -0,0 +1,9 @@
+package com.sirialkillers.shoponthego.Controllers;
+
+/**
+ * @author Ioakeim James Theologou
+ * @version 23/11/2017
+ *
+ */
+public class LoginController {
+}
---
+++ app/src/main/java/com/sirialkillers/shoponthego/Models/LoginModel.java
@@ -0,0 +1,9 @@
+package com.sirialkillers.shoponthego.Models;
+
+/**
+ * @author Ioakeim James Theologou
+ * @version 23/11/2017
+ *
+ */
+public class LoginModel {
+}
