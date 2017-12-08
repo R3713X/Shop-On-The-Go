b8bc98cd13923926aa1b64da0fddc05f804b5aea
R3713X <kapsalas_gi@yahoo.gr>
2017-12-08 04:55:47 +0200

    made the UI

---
+++ app/src/main/java/com/sirialkillers/shoponthego/Shop_Related_Activities/SeeMyShopsActivity.java
@@ -0,0 +1,18 @@
+package com.sirialkillers.shoponthego.Shop_Related_Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sirialkillers.shoponthego.R;
+
+public class SeeMyShopsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_my_shops);
+
+    }
+
+
+}
