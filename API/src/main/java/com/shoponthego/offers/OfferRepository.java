16a6c3fe88da00c11f26cd03fd099e2daecd40ee
dimiaman <dimiaman2@gmail.com>
2017-12-11 19:08:20 +0200

    Some changes in offer Service and addition of a repository

---
+++ API/src/main/java/com/shoponthego/offers/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.shoponthego.offers;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, String> {
+
+}
--- API/src/main/java/com/shoponthego/offers/OfferService.java
+++ API/src/main/java/com/shoponthego/offers/OfferService.java
@@ -1,38 +1,37 @@
 package com.shoponthego.offers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfferService {
 
-	
+	@Autowired
+	private OfferRepository offerRepository;
 	
 	public List<Offer> getAllOffers() {
-		/*
 		List<Offer> offers = new ArrayList<>();
-		offerRepository.findAll()
-		.forEach(offers::add);
+		offerRepository.findAll().forEach(offers::add);
 		return offers;
-		*/
-		return null;
 	}
 
 	public Offer getOfferById(String id) {
-		return null;
+		return offerRepository.findOne(id);
 		
 	}
 
 	public void addOffer(Offer offer) {
-		//offerRepository.save(offer);
+		offerRepository.save(offer);
 	}
 
 	public void updateOffer(String id, Offer newOffer) {
-		//offerRepository.save(newOffer);
+		offerRepository.save(newOffer);
 	}
 
 	public void deleteOffer(String id) {
-		//offerRepository.delete(id);
+		offerRepository.delete(id);
 	}
 }
