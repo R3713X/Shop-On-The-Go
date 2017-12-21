b3448d6adce02c3f934464308c8d988e67660ce0
R3713X <kapsalas_gi@yahoo.gr>
2017-12-21 02:07:39 +0200

    add_offer_activity_starting

---
+++ app/src/main/java/com/sirialkillers/shoponthego/Shop_Related_Activities/AddOfferActivity.java
@@ -0,0 +1,15 @@
+package com.sirialkillers.shoponthego.Shop_Related_Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sirialkillers.shoponthego.R;
+
+public class AddOfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_offer);
+    }
+}
