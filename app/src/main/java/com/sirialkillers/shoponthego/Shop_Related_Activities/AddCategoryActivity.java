86b434a685c08c0a75915c23f1e702386bfa3c2c
R3713X <kapsalas_gi@yahoo.gr>
2017-12-25 17:18:45 +0200

    add category UI

---
+++ app/src/main/java/com/sirialkillers/shoponthego/Shop_Related_Activities/AddCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.sirialkillers.shoponthego.Shop_Related_Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+    }
+}
